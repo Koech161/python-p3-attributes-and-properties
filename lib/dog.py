ddd2d8acc021d23a8c834cc746ceba5136ad175a
Abel Koech <koechabel161@gmail.com>
2024-08-29 20:27:48 +0300

    atrributes

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,38 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+
 class Dog:
-    pass
+    def __init__(self, name='Fido', breed='Mastiff'):
+        self.name = name
+        self.breed = breed
+
+    @property
+    def name(self):
+        """The name property"""
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        """Name must be a string between 1 and 25 characters in length"""
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name
+        else:
+               print("Name must be string between 1 and 25 characters.")
+            
+
+    @property
+    def breed(self):
+        """The breed property"""
+        return self._breed
+
+    @breed.setter
+    def breed(self, breed):
+        """Breed must be in the list of approved breeds"""
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+# fodi = Dog(name='Max', breed='Kali')
+# print(fodi)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,37 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    #pass
+    def __init__(self,name='Guido', job='ITC'):
+        self.name = name
+        self.job = job
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+
+    def name(self, name):
+          if isinstance(name, str) and 1 <= len(name) <= 25:
+               
+            self._name = name.title()
+          
+          else:
+               print('Name must be string between 1 and 25 characters.')
+    @property
+    def job(self):
+        return self._job
+    
+    @job.setter
+    def job(self, job):
+
+        """Job must be in the list of approved jobs"""
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print('Job must be in list of approved jobs.')   
+
+person1 = Person(name='john doe', job='Finance')
+print(f'Name: {person1.name}')  # Output: John Doe
+print(f'Job: {person1.job}')    # Output: Finance
+
+
